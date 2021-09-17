883a96bfb2c14f38451bdaa193059c01de216cd4
Raas Ahsan <raas.ahsan@gmail.com>
2021-09-16 19:08:54 -0500

    Separate main method

--- src/main/scala/example/Core.scala
+++ src/main/scala/example/Core.scala
@@ -116,33 +116,6 @@ object Core extends Syntax {
         case _ => throw new RuntimeException("invalid reification")
       }
 
-  def main(args: Array[String]): Unit = {
-    import Lists.{given, *}
-    import Nats.{given, *}
-
-    // val r1 = run[Nat] { x =>
-    //   fresh[Nat, Nat] { (y, z) => 
-    //     z === zero && y === succ(zero) && plus(z, x, y)
-    //   }
-    // }
-
-    // println(r1.take(10).toList)
-
-    val r2 = run[List[Int], List[Int]] { (x, y) =>
-      append(x, y, cons(int(1), cons(int(2), cons(int(3), cons(int(4), cons(int(5), nil))))))
-    }
-
-    // val r2 = run[Nat] { x =>
-    //   lte(zero, x)
-    // }
-
-    // val r2 = run[List[Int]] { x =>
-    //   reverse(x, cons(int(1), cons(int(2), cons(int(3), nil))))
-    // }
-
-    println(r2.take(10).toList)
-  }
-
   // TODO: can we abstract reification? so that we can reify recursive term references in any data structure
 
 }
---
+++ src/main/scala/example/interpreter/SimpleInterpreter.scala
@@ -0,0 +1,5 @@
+package example.interpreter
+
+object SimpleInterpreter {
+  
+}
