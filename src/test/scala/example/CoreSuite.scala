3def1303e9021e62bba62fc85004d329fcffe79a
Raas Ahsan <raas.ahsan@gmail.com>
2021-09-19 01:18:59 -0500

    Failing test for stream bind stack safety

--- src/test/scala/example/CoreSuite.scala
+++ src/test/scala/example/CoreSuite.scala
@@ -15,4 +15,22 @@ class CoreSuite extends FunSuite {
 
     assertEquals(result, List(1, 2, 1, 2))
   }
+
+  test("stream interleaving stack safety") {
+    val a = LazyList.continually(1)
+    val b = LazyList.continually(2)
+
+    val stream = interleave(a, b)
+    val size = stream.take(30000).size
+
+    assertEquals(size, 30000)
+  }
+
+  test("stream deep bind stack safety") {
+    def go(n: Int): LazyList[Int] =
+      if (n == 0) LazyList.empty[Int] else LazyList.apply(1).flatMap(_ => go(n - 1))
+
+    val size = go(30000).take(1).size
+    assertEquals(size, 1)
+  }
 }
